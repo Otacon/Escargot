ccd6135b983980eaa79840075530e73cf3c2ac7b
Orfeo <orfeo.ciano@imaginecurve.com>
2020-12-28 07:37:42 +0100

    Added status icons

---
+++ src/main/kotlin/features/contactList/ContactListCell.kt
@@ -0,0 +1,22 @@
+package features.contactList
+
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.scene.control.ListCell
+import java.io.IOException
+
+
+
+
+class ContactListCell : ListCell<ContactModel>() {
+
+    private fun loadFXML() {
+//        val resource = javaClass.getResource("/ItemContact.fxml")
+//        val root = FXMLLoader.load<Scene>(resource)
+//        stage.scene = root
+//        stage.isResizable = true
+//        bindViews(root)
+//        setupListeners()
+    }
+
+}
