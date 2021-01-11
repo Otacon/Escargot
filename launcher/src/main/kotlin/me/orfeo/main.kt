3a653524ce4e3f27494efce462bbcfc47175c53a
Orfeo <orfeo.ciano@imaginecurve.com>
2021-01-11 19:44:29 +0000

    Give native look and feel

--- launcher/src/main/kotlin/me/orfeo/main.kt
+++ launcher/src/main/kotlin/me/orfeo/main.kt
@@ -1,9 +1,15 @@
 package me.orfeo
 
 import me.orfeo.mainWindow.MainWindowView
+import javax.swing.UIManager
 
 
 fun main() {
+    try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (e: Exception){
+
+    }
     MainWindowView().show()
 }
 
